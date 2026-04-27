c2b9e686e6beedf39d1ba279db25e7e6d5c0e046
Golam Kibria <uchihai850@gmail.com>
2026-04-27 20:27:06 +0600

    Write mutations functions to store user in the convex database
    Signed-off-by: Golam Kibria <uchihai850@gmail.com>

--- components/header.jsx
+++ components/header.jsx
@@ -7,8 +7,12 @@ import React from 'react'
 import { Button } from './ui/button'
 import { Authenticated, Unauthenticated } from 'convex/react'
 import { BarLoader } from 'react-spinners'
+import { useStoreUser } from '@/hooks/use-store-user'
 
 const Header = () => {
+
+  const { isLoading } = useStoreUser();
+
   return (
     <>
       <nav className='fixed top-0 left-0 right-0 bg-background/80 backdrop-blur-xl z-20 border-b'>
@@ -50,9 +54,9 @@ const Header = () => {
         {/* Mobile Search & Location - Below Header */}
 
         {/* Loader */}
-        <div className='absolute bottom-0 left-0 w-full'>
+        {isLoading && <div className='absolute bottom-0 left-0 w-full'>
           <BarLoader width={'100%'} color="#ff4b33" />
-        </div>
+        </div>}
       </nav>
 
       {/* Modals */}
