e839cf0fad3fa78d4c43c8bfc98ca8d98daca41b
Golam Kibria <uchihai850@gmail.com>
2026-04-28 05:19:01 +0600

    Add header(nav) menus and user links
    Signed-off-by: Golam Kibria <uchihai850@gmail.com>

--- components/header.jsx
+++ components/header.jsx
@@ -3,15 +3,17 @@
 import { SignInButton, SignUpButton, UserButton } from '@clerk/nextjs'
 import Image from 'next/image'
 import Link from 'next/link'
-import React from 'react'
+import React, { useState } from 'react'
 import { Button } from './ui/button'
 import { Authenticated, Unauthenticated } from 'convex/react'
 import { BarLoader } from 'react-spinners'
 import { useStoreUser } from '@/hooks/use-store-user'
+import { Building, Plus, Ticket } from 'lucide-react'
 
 const Header = () => {
 
   const { isLoading } = useStoreUser();
+  const [showUpgradeModal, setShowUpgradeModal] = useState(false);
 
   return (
     <>
@@ -36,17 +38,45 @@ const Header = () => {
           {/* Right Side Actions */}
           <div className='flex items-center'>
 
-            <Authenticated>
-              {/* Create Event */}
+            <Button variant={'ghost'} size='sm' onClick={setShowUpgradeModal}>
+              Pricing
+            </Button>
 
-              <UserButton />
+            <Button variant='ghost' size='sm' asChild className={'mr-2'}>
+              <Link href="explore">Explore</Link>
+            </Button>
+
+            <Authenticated>
+              <Button size='sm' asChild className={'flex gap-2 mr-4'}>
+                <Link href="/create-event">
+                  <Plus className='w-4 h-4' />
+                  <span className='hidden sm:inline'>Create Event</span>
+                </Link>
+              </Button>
+
+              <UserButton>
+                <UserButton.MenuItems>
+                  <UserButton.Link 
+                    label="My Tickets"
+                    labelIcon={<Ticket size={16}/>}
+                    href="/my-tickets"
+                  />
+
+                  <UserButton.Link 
+                    label="My Events"
+                    labelIcon={<Building size={16}/>}
+                    href="/my-events"
+                  />
+
+                  <UserButton.Action label="manageAccount"/>
+                </UserButton.MenuItems>
+              </UserButton>
             </Authenticated>
 
             <Unauthenticated>
               <SignInButton mode="modal">
                 <Button size="sm">Sign In</Button>
               </SignInButton>
-              
             </Unauthenticated>
           </div>
         </div>
