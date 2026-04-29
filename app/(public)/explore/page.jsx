55bb05d4fd8d8bf78677ca0656181c045c9f0cbf
Golam Kibria <uchihai850@gmail.com>
2026-04-29 16:04:41 +0600

    Add featured events description | explore page
    Signed-off-by: Golam Kibria <uchihai850@gmail.com>

--- app/(public)/explore/page.jsx
+++ app/(public)/explore/page.jsx
@@ -1,9 +1,12 @@
 "use client"
 
+import { Badge } from "@/components/ui/badge";
 import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from "@/components/ui/carousel";
 import { api } from "@/convex/_generated/api"
 import { useConvexQuery } from "@/hooks/use-convex-query"
+import { format } from "date-fns";
 import Autoplay from "embla-carousel-autoplay";
+import { Calendar, MapPin, Users } from "lucide-react";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 import { useRef } from "react";
@@ -84,12 +87,49 @@ const ExplorePage = () => {
                         }}
                       />
                     )}
+
+                    <div className="absolute inset-0 bg-linear-to-r from-black/60 to-black/30" />
+
+                    <div className="relative h-full flex flex-col justify-end p-8 md:p-12">
+                      <Badge className="w-fit mb-4" variant="secondary">
+                        {event.category}, {event.state || event.country}
+                      </Badge>
+                      <h2 className="text-3xl md:text-5xl font-bold mb-3 text-white">
+                        {event.title}
+                      </h2>
+                      <p className="text-sm text-white/90 mb-4 max-w-2xl line-clamp-2">
+                        {event.description}
+                      </p>
+
+                      <div className="flex items-center gap-4 text-white/80">
+                        <div className="flex items-center gap-2">
+                          <Calendar className="w-4 h-4" />
+                          <span className="text-sm">
+                            {format(event.startDate, "ppp")}
+                          </span>
+                        </div>
+
+                        <div className="flex items-ceter gap-2">
+                          <MapPin className="w-4 h-4" />
+                          <span className="text-sm">
+                            {event.city}
+                          </span>
+                        </div>
+
+                        <div className="flex items-ceter gap-2">
+                          <Users className="w-4 h-4" />
+                          <span className="text-sm">
+                            {event.registrationCount} registered
+                          </span>
+                        </div>
+                      </div>
+                    </div>
                   </div>
                 </CarouselItem>
               ))}
             </CarouselContent>
-            <CarouselPrevious />
-            <CarouselNext />
+            <CarouselPrevious className="left-3" />
+            <CarouselNext className="right-3" />
           </Carousel>
         </div>
       }
