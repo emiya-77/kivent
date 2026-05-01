5ee526cd5d5c227fc59fedf750a0a1b21ae375fc
Golam Kibria <uchihai850@gmail.com>
2026-05-01 16:54:03 +0600

    Working on dynamic explore page
    Signed-off-by: Golam Kibria <uchihai850@gmail.com>

--- app/(public)/explore/[slug]/page.jsx
+++ app/(public)/explore/[slug]/page.jsx
@@ -1,6 +1,23 @@
-import React from 'react'
+"use client"
+
+import { CATEGORIES } from '@/lib/data';
+import { parseLocationSlug } from '@/lib/locations-utils';
+import { useParams, useRouter } from 'next/navigation';
 
 const DynamicExplorePage = () => {
+  const params = useParams();
+  const router = useRouter();
+  const slug = params.slug;
+
+  // Check if category is valid
+  const categoryInfo = CATEGORIES.find((cat) => cat.id == slug);
+  const isCategory = !!categoryInfo;
+
+  // if not a category, validate location
+  const { city, state, isValid } = !isCategory
+    ? parseLocationSlug(slug)
+    : { city: null, state: null, isValid: true };
+
   return (
     <div>DynamicExplorePage</div>
   )
